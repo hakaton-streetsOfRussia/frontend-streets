6c4e94440d279383fe3803aaac5c315b0e26391f
Daniil <kashel369@bk.ru>
2024-05-15 06:00:38 +0300

    feat: Fonts added + fix: little css fixes + todos in appHeader component

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,36 +1,34 @@
-import React, { useState } from "react";
-import styles from "./AppHeader.module.scss";
-import { ReactComponent as Logo } from "../../images/logo.svg";
-import { ReactComponent as Search } from "../../images/serach.svg";
-import { ReactComponent as Justify } from "../../images/justify.svg";
-import { ReactComponent as Cross } from "../../images/cross.svg";
+import { useState } from 'react'
+import { ReactComponent as Cross } from '../../images/cross.svg'
+import { ReactComponent as Justify } from '../../images/justify.svg'
+import { ReactComponent as Logo } from '../../images/logo.svg'
+import { ReactComponent as Search } from '../../images/serach.svg'
+import styles from './AppHeader.module.scss'
 
 function AppHeader() {
-  const [menuVisible, setMenuVisible] = useState<boolean>(false);
+  const [menuVisible, setMenuVisible] = useState<boolean>(false)
 
   const toggleMenu = () => {
-    setMenuVisible(!menuVisible);
-  };
+    setMenuVisible(!menuVisible)
+  }
 
   return (
     <header className={styles.header}>
       <div className={styles.ddd}>
-        <Logo />
+        {/* would be link - to the main page */}
+        <div className={styles.logoWrapper}>
+          <Logo />
+        </div>
         <div className={styles.search_bar}>
           <form className={styles.form}>
-            <input
-              className={styles.search}
-              type="text"
-              name="query"
-              placeholder="Выберите свой регион"
-              required
-            />
+            <input className={styles.search} type="text" name="query" placeholder="Выберите свой регион" required />
             <button type="submit" className={styles.button}>
               <Search />
             </button>
           </form>
         </div>
         <button className={styles.registration}>Зарегистрироваться</button>
+        {/* todo need to fix colors and for easy icons maybe should use React Icons (because of now Justify icon cant handle color) */}
         <button className={styles.menu} onClick={toggleMenu}>
           {!menuVisible ? <Justify /> : <Cross />}
         </button>
@@ -57,7 +55,7 @@ function AppHeader() {
         </nav>
       )}
     </header>
-  );
+  )
 }
 
-export default AppHeader;
+export default AppHeader
