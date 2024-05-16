f1610acac17f4e22e040814da4a8bbe8bf743489
Daniil <kashel369@bk.ru>
2024-05-16 05:59:31 +0300

    feat: add craco, made Title (video with name) Fix: some fixes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import styles from "./App.module.scss";
-import AppHeader from "../AppHeader/AppHeader";
+import AppHeader from '../AppHeader/AppHeader'
+import { Title } from '../Title'
+import styles from './App.module.scss'
 
 function App() {
   return (
     <>
       <AppHeader />
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Title videoUrl={'https://youtu.be/9rzrFQdE7JM'} />
+      </main>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,8 +1,8 @@
+import { ReactComponent as Cross } from '@/images/cross.svg'
+import { ReactComponent as Justify } from '@/images/justify.svg'
+import { ReactComponent as Logo } from '@/images/logo.svg'
+import { ReactComponent as Search } from '@/images/search.svg'
 import { useState } from 'react'
-import { ReactComponent as Cross } from '../../images/cross.svg'
-import { ReactComponent as Justify } from '../../images/justify.svg'
-import { ReactComponent as Logo } from '../../images/logo.svg'
-import { ReactComponent as Search } from '../../images/serach.svg'
 import styles from './AppHeader.module.scss'
 
 function AppHeader() {
@@ -15,7 +15,6 @@ function AppHeader() {
   return (
     <header className={styles.header}>
       <div className={styles.ddd}>
-        {/* would be link - to the main page */}
         <div className={styles.logoWrapper}>
           <Logo />
         </div>
@@ -28,7 +27,6 @@ function AppHeader() {
           </form>
         </div>
         <button className={styles.registration}>Зарегистрироваться</button>
-        {/* todo need to fix colors and for easy icons maybe should use React Icons (because of now Justify icon cant handle color) */}
         <button className={styles.menu} onClick={toggleMenu}>
           {!menuVisible ? <Justify /> : <Cross />}
         </button>
