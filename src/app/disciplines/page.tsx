f96ef9d7146be107c6e1f63f155ca44b40263ca5
Daniil <kashel369@bk.ru>
2024-05-20 15:40:46 +0300

    feat: FiraSans localy was added + half of desciplines page

--- src/app/disciplines/page.tsx
+++ src/app/disciplines/page.tsx
@@ -1,3 +1,20 @@
+import StreetArtDemoPath from '@/public/images/street-art-demo.jpg'
+import Image from 'next/image'
+import style from './style.module.scss'
 export default function Disciplines() {
-  return <main>Im here!</main>
+  return (
+    <main>
+      <h2 className={style.title}>Дисциплины</h2>
+      <button>Street Art</button>
+      <div className={style.demoWrapper}>
+        <p>
+          Стрит-арт — это разновидность современного урбанистического искусства. Бытует широкое заблуждение, что граффити является единственным
+          проявлением стрит-арт. Однако, это не так, граффити является лишь одним из видов уличного искусства, но далеко не единственным. Разделение
+          на стили можно наблюдать, в основном, среди граффити: writing, bombing, tagging, bubble-letter, throw-up, character, wild style,
+          3D-style.Стрит-арт своеобразный способ выразить себя и своё творчество, а также самоутвердиться в обществе.
+        </p>
+        <Image src={StreetArtDemoPath} alt="demo" />
+      </div>
+    </main>
+  )
 }
