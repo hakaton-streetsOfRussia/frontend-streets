009e84ea477aba755a1201286125deb2a93d21b5
Lev Smirnov <levsmirnov2015@mail.ru>
2024-05-23 12:50:16 +0500

    add page calendar

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -2,9 +2,16 @@
 import { useState } from "react";
 import { Calendar } from "./_components/Calendar";
 import style from "./style.module.scss";
+import { Dropdown } from "./_components/Dropdown";
+import { EventCard } from "./_components/Eventcard";
 
 export default function Calendarevents() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [selectedRegion, setSelectedRegion] = useState<string | null>(null);
+  const [selectedEvent, setSelectedEvent] = useState<string | null>(null);
+  const [selectedDiscipline, setSelectedDiscipline] = useState<string | null>(
+    null
+  );
 
   const handleDateSelect = (date: Date | null) => {
     setSelectedDate(date);
@@ -19,12 +26,77 @@ export default function Calendarevents() {
             <Calendar onDateSelect={handleDateSelect} />
           </div>
           <div className={style.dropdownmenu}>
-            <div className={style.menu}></div>
-            <div className={style.menu}></div>
-            <div className={style.menu}></div>
+            <Dropdown
+              label="УКАЖИТЕ РЕГИОН"
+              options={[
+                "Московская область",
+                "хмао",
+                "Ивановская область",
+                "Ростовская область",
+                "Калининградская область",
+                "Амурская область",
+                "Алтайский край",
+              ]}
+              onSelect={setSelectedRegion}
+              selectedOption={selectedRegion}
+            />
+
+            <Dropdown
+              label="ВЫБЕРИТЕ СОБЫТИЕ"
+              options={["ТРЕНИРОВКА", "СОРЕВНОВАНИЕ", "ВЫСТУПЛЕНИЯ"]}
+              onSelect={setSelectedEvent}
+              selectedOption={selectedEvent}
+            />
+
+            <Dropdown
+              label="ВЫБЕРИТЕ ДИСЦИПЛИНУ"
+              options={[
+                "Parkour",
+                "Workout",
+                "Бмх",
+                "Скейтбординг",
+                "Трюковой самокат",
+                "Фриран",
+                "Трикинг",
+                "Брейк-данс",
+                "Граффити",
+                "Диджеинг",
+                "Реп",
+              ]}
+              onSelect={setSelectedDiscipline}
+              selectedOption={selectedDiscipline}
+            />
           </div>
         </div>
-        <div className={style.events}></div>
+        <div className={style.events}>
+          <div className={style.event}>
+            <EventCard
+              region="Амурская область г. Свободный"
+              event="Тренировка"
+              direction="Воркаут"
+              imageSrc="/images/calendar/event.png"
+              imageAlt="Example image"
+            />
+          </div>
+          <div className={style.event}>
+            <EventCard
+              region="Амурская область г. Свободный"
+              event="Тренировка"
+              direction="Воркаут"
+              imageSrc="/images/calendar/event.png"
+              imageAlt="Example image"
+            />
+          </div>
+          <div className={style.event}>
+            <EventCard
+              region="Амурская область г. Свободный"
+              event="Тренировка"
+              direction="Воркаут"
+              imageSrc="/images/calendar/event.png"
+              imageAlt="Example image"
+            />
+          </div>
+        </div>
       </div>
     </main>
   );
