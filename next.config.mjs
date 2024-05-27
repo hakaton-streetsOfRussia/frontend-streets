19b126a8e126fc42bc0a014a3b3137d1fec6042c
Daniil <kashel369@bk.ru>
2024-05-27 17:25:58 +0300

    removed base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
-  basePath: isProd ? '/frontend-streets' : '',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
