b843a9f8240c20fb13827543a5b368e777d8b54d
Daniil <kashel369@bk.ru>
2024-05-27 17:23:08 +0300

    removed static deploy commands

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,30 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
+  basePath: isProd ? '/frontend-streets' : '',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
+      use: ['@svgr/webpack'],
+    })
+    return config
   },
   async redirects() {
     return [
       {
-        source: "/",
-        destination: "/home",
+        source: '/',
+        destination: '/home',
         permanent: true,
       },
       {
-        source: "/disciplines",
-        destination: "/disciplines/street-art",
+        source: '/disciplines',
+        destination: '/disciplines/street-art',
         permanent: true,
       },
-    ];
+    ]
   },
-  basePath: isProd ? "/frontend-streets" : "",
-  output: "export",
-};
+}
 
-export default nextConfig;
+export default nextConfig
